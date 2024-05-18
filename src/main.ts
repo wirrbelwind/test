a38f50b963a4c8a4d67542473a9bf56c38448cf6
Igor Komendantov <komendantov2580@gmail.com>
2024-05-18 21:07:46 +0300

    BE-feat: creage swagger document & endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const swaggerConfig = new DocumentBuilder().setTitle('Swagger title').build()
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('api', app, swaggerDocument)
+
   await app.listen(3000);
 }
 bootstrap();
