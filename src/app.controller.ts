452507cb6d58db62b4d237b4f0121dba282678ae
Igor Komendantov <komendantov2580@gmail.com>
2024-05-18 21:53:39 +0300

    BE-feat: use db service in app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
+import { DbService } from './db/db.service';
+
+class HelloWorldDto {
+  @ApiProperty()
+  message: string
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private dbService: DbService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOkResponse({
+    type: HelloWorldDto
+  })
+  async getHello(): Promise<HelloWorldDto> {
+    const users = await this.dbService.user.findMany({})
+    console.log(users);
+
+    return { message: this.appService.getHello() }
   }
 }
