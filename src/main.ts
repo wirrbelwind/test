d500c4095dc677771d1aa3d312457df5bf2fa089
Igor Komendantov <komendantov2580@gmail.com>
2024-05-25 18:15:32 +0300

    BE-feat: implement cookie parser to app

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   const swaggerConfig = new DocumentBuilder().setTitle('Swagger title').build()
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig)
   SwaggerModule.setup('api', app, swaggerDocument)
+
+  app.use(cookieParser())
 
   await app.listen(3000);
 }
