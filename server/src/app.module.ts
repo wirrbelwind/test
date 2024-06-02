44c75d2dcd5b4c9aa4de464b44b000d56adcbd9e
Igor Komendantov <komendantov2580@gmail.com>
2024-06-02 17:56:00 +0300

    BE-feature: init graphql module in app

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,6 +6,7 @@ import { DbModule } from './db/db.module';
 // import { AccountModule } from './account/account.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -14,6 +15,8 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
     // AccountModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
+      sortSchema: true
     }),
   ],
   controllers: [AppController],
