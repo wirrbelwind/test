23c9315927d917cc6b46c26ce408b69e1301ebaa
Igor Komendantov <komendantov2580@gmail.com>
2024-07-13 02:05:34 +0300

    Connect auth module to root

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,24 +1,23 @@
 import { Module } from '@nestjs/common';
 import { DbModule } from './db/db.module';
-import { AuthModule } from './auth/auth.module';
-import { AccountModule } from './account/account.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     DbModule,
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
+    //   sortSchema: true,
+    //   playground: true,
+    // }),
     AuthModule,
-    AccountModule,
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
-      sortSchema: true,
-      playground: true,
-    }),
   ],
-  controllers: [],
+  controllers: [
+  ],
   providers: [],
 })
 export class AppModule { }
