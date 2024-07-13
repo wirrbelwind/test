627135967b4c8bbc407581ca9216914ea206693a
Igor Komendantov <komendantov2580@gmail.com>
2024-07-13 14:49:30 +0300

    BE migrate to code first in graphql

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,18 +2,17 @@ import { Module } from '@nestjs/common';
 import { DbModule } from './db/db.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
+import { join } from 'path';
 
 @Module({
   imports: [
     DbModule,
-    // GraphQLModule.forRoot<ApolloDriverConfig>({
-    //   driver: ApolloDriver,
-    //   autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
-    //   sortSchema: true,
-    //   playground: true,
-    // }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'graphql/schema.gql'),
+      sortSchema: true,
+    }),
     AuthModule,
   ],
   controllers: [
