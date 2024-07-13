beb27a18fc5f771037671cfcb714d8fba4fb2c7f
Igor Komendantov <komendantov2580@gmail.com>
2024-07-13 12:55:58 +0300

    Init auth resolver

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthTokenService } from './auth-token.service';
+import { AuthResolver } from './auth.resolver';
 
 @Module({
-  providers: [AuthService, AuthTokenService],
+  providers: [AuthService, AuthTokenService, AuthResolver],
   imports: [
     JwtModule.register({
       global: true,
